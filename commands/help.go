ce5dd104d90ff0aa9dc1376b8bdd1b6f58180d59
zenith110 <thequake@live.com>
2020-09-28 21:58:50 -0400

    Added some more info to help

--- commands/help.go
+++ commands/help.go
@@ -13,8 +13,9 @@ import (
 
 func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
 	message := "```Greetings, I am Xatu. I am can both predict the future, and guide the present. Below are what I offer." + 
-	"\nPubsubs - You crave these sandwiches, and I shall predict when they are on sale!" +
-	"\nFE - I see you are studying for the FE, very well. Using this command, you will be allowed to get data on whichever FE you choose! I offer stats, as well as links to the exams and their solutions for your convience." + 
-	"\nYou may find my source code here: ```"
+	"\n!pubsub - You crave these sandwiches, and I shall predict when they are on sale!" +
+	"\n!FE - I see you are studying for the FE, very well. Using this command, you will be allowed to get data on whichever FE you choose! I offer stats, as well as links to the exams and their solutions for your convience." + 
+	"\n!countdown - To see how much time is left till the next FE exam is, work dilligently." +
+	"\nYou may find my source code here: https://github.com/zenith110/FE_bot if you want to see my innerworkings.```"
 	s.ChannelMessageSend(m.ChannelID, message)		
 }
