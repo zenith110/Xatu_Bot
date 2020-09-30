1b1370faf80ddfd873ff4d18675e23edfe38bebf
zenith110 <thequake@live.com>
2020-09-30 11:24:25 -0400

    Added some safe guards

--- commands/help.go
+++ commands/help.go
@@ -16,6 +16,6 @@ func Help(s *discordgo.Session, m *discordgo.MessageCreate) {
 	"\n!pubsub - You crave these sandwiches, and I shall predict when they are on sale!" +
 	"\n!FE - I see you are studying for the FE, very well. Using this command, you will be allowed to get data on whichever FE you choose! I offer stats, as well as links to the exams and their solutions for your convience." + 
 	"\n!countdown - To see how much time is left till the next FE exam is, work dilligently." +
-	"\nYou may find my source code here: https://github.com/zenith110/FE_bot if you want to see my innerworkings.```"
+	"\nYou may find my source code here: https://github.com/zenith110/Xatu_Bot if you want to see my innerworkings.```"
 	s.ChannelMessageSend(m.ChannelID, message)		
 }
--- commands/pubsub.go
+++ commands/pubsub.go
@@ -19,15 +19,15 @@ func jsonParser(textMatch string, text string) string {
 }
 
 func Pubsub(s *discordgo.Session, m *discordgo.MessageCreate)int{
+			// If we have a secondary argument continue down this conditional
 			if(len(m.Content) > 7) {
-			// client := &http.Client{}
 			// Grabs the name from the user who inputted it
 			secondaryArgs := m.Content[8:]
 			// Lowercases it for ease of use into the database
 			secondaryArgsLower := strings.ToLower(secondaryArgs)
+
 			// Removes all spaces in the name
 			finalArg := strings.Replace(secondaryArgsLower, " ", "-", -1)
-			fmt.Println(finalArg)
 			fetchurl := "https://pubsub-api.dev/subs/?name=" + finalArg
 
 			// Sends a post request to the url above
@@ -40,16 +40,17 @@ func Pubsub(s *discordgo.Session, m *discordgo.MessageCreate)int{
 			bodyData, err := ioutil.ReadAll(req.Body)
 			bodyText := string(bodyData)
 			// Returns an error whenever we get something that's not in the database
-			// if len(bodyData) < 300{
-			// 	s.ChannelMessageSend(m.ChannelID, "It seems that " + secondaryArgsLower + " is currently not in the database!\nPlease try again later")
-			// 	return 0
-			// }
+			if len(bodyData) < 300{
+				s.ChannelMessageSend(m.ChannelID, "It seems that " + secondaryArgsLower + " is currently not in the database!\nPlease try again later")
+				return 0
+			}
 
 			// // Parses the text for the subname
 			subname_pattern := regexp.MustCompile(`.*"sub_name": .*`)
 			subname_match := subname_pattern.FindStringSubmatch(string(bodyText))
 			sub_name := jsonParser(subname_match[0], "sub_name")
 			sub_name = strings.Replace(sub_name, `-`, " ", -1)
+
 			last_sale_pattern := regexp.MustCompile(`.*"last_sale": .*`)
 			last_sale_match := last_sale_pattern.FindStringSubmatch(string(bodyText))
 			last_sale := jsonParser(last_sale_match[0], "last_sale")
@@ -99,12 +100,87 @@ func Pubsub(s *discordgo.Session, m *discordgo.MessageCreate)int{
 
 				Timestamp: time.Now().Format(time.RFC3339), // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
 			}
-			if(status_sale == "True"){
+			// Checks if currently on sale or not
+			if(status_sale == " True"){
 				// Sends message
 				s.ChannelMessageSendEmbed(m.ChannelID, onSaleEmbed)
 			}else{
 				s.ChannelMessageSendEmbed(m.ChannelID, notOnSaleEmbed)
 			}
+	}else{
+		fetchurl := "https://pubsub-api.dev/subs/?name="
+
+			// Sends a post request to the url above
+			req, err := http.Get(fetchurl)
+			// Will always be NIL, ignore
+			if err == nil{
+			}
+			
+			// // Puts the body text bytes to be read into a variable so we can check length
+			bodyData, err := ioutil.ReadAll(req.Body)
+			bodyText := string(bodyData)
+
+			// // Parses the text for the subname
+			subname_pattern := regexp.MustCompile(`.*"sub_name": .*`)
+			subname_match := subname_pattern.FindStringSubmatch(string(bodyText))
+			sub_name := jsonParser(subname_match[0], "sub_name")
+			sub_name = strings.Replace(sub_name, `-`, " ", -1)
+			last_sale_pattern := regexp.MustCompile(`.*"last_sale": .*`)
+			last_sale_match := last_sale_pattern.FindStringSubmatch(string(bodyText))
+			last_sale := jsonParser(last_sale_match[0], "last_sale")
+
+			status_pattern := regexp.MustCompile(`.*"status": .*`)
+			status_match := status_pattern.FindStringSubmatch(string(bodyText))
+			status_sale := jsonParser(status_match[0], "status")
+
+			price_pattern := regexp.MustCompile(`.*"price": .*`)
+			price_match := price_pattern.FindStringSubmatch(string(bodyText))
+			price := jsonParser(price_match[0], "price")
+
+			image_pattern := regexp.MustCompile(`.*"image": .*`)
+			image_match := image_pattern.FindStringSubmatch(string(bodyText))
+			image := jsonParser(image_match[0], "image")
+			fmt.Println(status_sale)
+			notOnSaleEmbed := &discordgo.MessageEmbed{
+				Color: 0x00ff00, // Green
+				Fields: []*discordgo.MessageEmbedField{
+					&discordgo.MessageEmbedField{
+						Name:   sub_name,
+						Value:  "Last seen on sale from " + last_sale + " for " + price,
+						Inline: true,
+					},
+				},
+
+				Image: &discordgo.MessageEmbedImage{
+					URL: image,
+				},
+
+				Timestamp: time.Now().Format(time.RFC3339), // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
+			}
+
+			onSaleEmbed := &discordgo.MessageEmbed{
+				Color: 0x00ff00, // Green
+				Fields: []*discordgo.MessageEmbedField{
+					&discordgo.MessageEmbedField{
+						Name:   sub_name,
+						Value:  "Currently on sale from " + last_sale + " for " + price,
+						Inline: true,
+					},
+				},
+
+				Image: &discordgo.MessageEmbedImage{
+					URL: image,
+				},
+
+				Timestamp: time.Now().Format(time.RFC3339), // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
+			}
+			if(status_sale == " True"){
+				// Sends message
+				s.ChannelMessageSendEmbed(m.ChannelID, onSaleEmbed)
+			}else{
+				s.ChannelMessageSendEmbed(m.ChannelID, notOnSaleEmbed)
+			}
+			
 	}
 	return 0
 }
