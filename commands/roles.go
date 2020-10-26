14b83e54054538dfd5dddb1a8db2545658a6a168
zenith110 <thequake@live.com>
2020-10-26 17:41:06 -0400

    Added role help list

--- commands/roles.go
+++ commands/roles.go
@@ -2,9 +2,10 @@ package commands
 
 import
 (
-	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"strings"
 )
+
 func Role_ID_Finder(guild_roles []*discordgo.Role, role string) string{
 	for i := 0; i < len(guild_roles); i++{
 		if(guild_roles[i].Name == role){
@@ -18,17 +19,28 @@ func Role_ID_Finder(guild_roles []*discordgo.Role, role string) string{
 // Includes assigning custom channels
 func Role_Caller(s *discordgo.Session, m *discordgo.MessageCreate, BotID string){
 	if(len(m.Content) > 5){
+		nickname := m.Author.Mention()
 		role := m.Content[6:]
-		// Loop through the guild struct of the server to find the role data
-		guild, guild_err := s.State.Channel(m.ChannelID)
-		// Assigns the roles using the GuildID
-		guild_roles, err := s.GuildRoles(guild.GuildID)
-		if(err != nil){
+		if(strings.Contains(role, "help")){
+			s.ChannelMessageSend(m.ChannelID,  nickname + ", the current roles available are: Leetcode, and Among us")
+		}else{
+			// Loop through the guild struct of the server to find the role data
+			guild, guild_err := s.State.Channel(m.ChannelID)
+			// Assigns the roles using the GuildID
+			guild_roles, err := s.GuildRoles(guild.GuildID)
+			if(err != nil){
+			}
+			if(guild_err != nil){
+			}
+			role_id := Role_ID_Finder(guild_roles, role)
+			// Sanity check for a role cannot be assigned, send a message
+			edit := s.GuildMemberRoleAdd(guild.GuildID, m.Author.ID, string(role_id))
+
+			if(edit != nil){
+				s.ChannelMessageSend(m.ChannelID, "Sorry " + nickname + ", unable to assign " + role + ", try another one!")	
+			}else{
+			s.ChannelMessageSend(m.ChannelID,  nickname + ", the role " + role + " has been assigned!")
+			}
 		}
-		if(guild_err != nil){
-		}
-		role_id := Role_ID_Finder(guild_roles, role)
-		edit := s.GuildMemberRoleAdd(guild.GuildID, m.Author.ID, string(role_id))
-		fmt.Println(edit)
 	}
 }
